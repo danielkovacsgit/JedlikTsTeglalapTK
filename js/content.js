7bd2d5890d8acb0777f40180933523162691d3ad
danielkovacsgit <daniel.kovacs1771@gmail.com>
2019-09-17 09:22:14 +0200

    up

--- js/content.js
+++ js/content.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const url = require("url");
 class Content {
     content(req, res) {
         // favicon.ico kérés kiszolgálása:
@@ -12,15 +13,22 @@ class Content {
         res.write("<!DOCTYPE html>");
         res.write("<html lang ='hu'>");
         // A weboldal fejléce
-        res.write("<head>");
-        res.write("<title>TeglalapTK</title>");
-        res.write("</head>");
-        res.write("<body>");
+        res.write("<head><title>TeglalapTK</title></head>");
+        res.write("<body><form style = 'font-family:Courier; font-size: 24px'>");
         res.write("<h1>Téglalap területe és kerülete</h1>");
-        const a = 5; // number --> 64 bites lebegőpontos szám
-        const b = 5;
-        res.write(`<h2>a=${a}</h2>`);
-        res.write("</body> </html>");
+        const querry = url.parse(req.url, true).query;
+        const a = querry.aInput === undefined || querry.aInput === "" ? 5 : parseFloat(querry.aInput);
+        const b = querry.bInput === undefined || querry.bInput === "" ? 6 : parseFloat(querry.bInput);
+        res.write("<p>a= ");
+        res.write(`<input type= 'number' name="aInput" value=${a} onChange='this.form.submit=();'></p>`);
+        res.write("<p>b= ");
+        res.write(`<input type= 'number' name="bInput" value=${b} onChange='this.form.submit=();'></p>`);
+        res.write(`<p2>b=${b}</p2>`);
+        const terulet = a * b;
+        const kerulet = 2 * (a * b);
+        res.write(`<p2>a=${terulet}</p2>`);
+        res.write(`<p2>a=${kerulet}</p2>`);
+        res.write("</form></body> </html>");
         res.end();
     }
 }
--- ts/content.ts
+++ ts/content.ts
@@ -1,7 +1,7 @@
 ﻿import * as fs from "fs";
 import * as http from "http";
-import * as path from "path";
-
+import { ParsedUrlQuery } from "querystring";
+import * as url from "url";
 export default class Content {
 
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
@@ -16,18 +16,23 @@ export default class Content {
         res.write("<!DOCTYPE html>");
         res.write("<html lang ='hu'>");
         // A weboldal fejléce
-        res.write("<head>");
-        res.write("<title>TeglalapTK</title>");
-
-        res.write("</head>");
-        res.write("<body>");
+        res.write("<head><title>TeglalapTK</title></head>");
+        res.write("<body><form style = 'font-family:Courier; font-size: 24px'>");
 
         res.write("<h1>Téglalap területe és kerülete</h1>");
-        const a: number = 5; // number --> 64 bites lebegőpontos szám
-        const b: number = 5;
-        res.write(`<h2>a=${a}</h2>`);
-
-        res.write("</body> </html>");
+        const querry: ParsedUrlQuery = url.parse(req.url, true).query;
+        const a: number = querry.aInput === undefined || querry.aInput === "" ? 5 : parseFloat(querry.aInput as string);
+        const b: number = querry.bInput === undefined || querry.bInput === "" ? 6 : parseFloat(querry.bInput as string);
+        res.write("<p>a= ");
+        res.write(`<input type= 'number' name="aInput" value=${a} onChange='this.form.submit=();'></p>`);
+        res.write("<p>b= ");
+        res.write(`<input type= 'number' name="bInput" value=${b} onChange='this.form.submit=();'></p>`);
+        res.write(`<p2>b=${b}</p2>`);
+        const terulet: number = a * b;
+        const kerulet: number = 2 * (a * b);
+        res.write(`<p2>a=${terulet}</p2>`);
+        res.write(`<p2>a=${kerulet}</p2>`);
+        res.write("</form></body> </html>");
         res.end();
     }
 }
